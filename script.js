628fd4287792ef58d0738a1ee07ea7a411ad290b
DimaCherkes <dmcerkes99@gmai.com>
2024-12-22 19:24:03 +0100

    bugfix

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const ctx = canvas.getContext('2d');
 // Размеры Canvas
 const WIDTH = canvas.width;
 const HEIGHT = canvas.height;
+const SQUARE_SIZE = 15;
 
 // Параметры игры
 let gravity = 0.1;     // Гравитация
